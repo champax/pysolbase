f3f69ecad57341c619d27b0b6ddacb10072355b5
champax <champagnac.laurent@gmail.com>
2022-05-23 14:48:08 +0000

    Fix resource warning

--- pysolbase/SolBase.py
+++ pysolbase/SolBase.py
@@ -605,12 +605,24 @@ class SolBase(object):
         # Initialize
         root = logging.getLogger()
         root.setLevel(logging.getLevelName("INFO"))
+        for h in root.handlers:
+            # noinspection PyBroadException
+            try:
+                h.close()
+            except:
+                pass
         root.handlers = []
 
         # Browse all loggers and set
         for name in logging.root.manager.loggerDict:
             cur_logger = logging.getLogger(name)
             cur_logger.setLevel(logging.getLevelName("INFO"))
+            for h in cur_logger.handlers:
+                # noinspection PyBroadException
+                try:
+                    h.close()
+                except:
+                    pass
             cur_logger.handlers = []
 
     @classmethod
